30ea051697fc2a9a85cd160aec94bf70c28af69f
lua1235 <124845051+lua1235@users.noreply.github.com>
2023-03-10 09:10:19 -0700

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-const button = document.getElementById("sub")
-function update() {
-    let redValue = document.getElementById("red").value
-    let greenValue = document.getElementById("green").value
-    let blueValue = document.getElementById("blue").value
-    document.getElementById("color-box").style.fill ="rgb("+redValue+", "+greenValue+", "+blueValue+")"
-}
